dd859868a5b926816ef2995def217af411fbfe5a
Giovanni Ricca <giovanniricca@proton.me>
2022-09-15 20:56:58 +0530

    vili on hwid
    Change-Id: I18cb494493a7272671c2e1c778b86f98a079edc9

--- drivers/misc/hwid.c
+++ drivers/misc/hwid.c
@@ -86,8 +86,10 @@ const char *product_name_get(void)
 		case HARDWARE_PROJECT_K1:  return "star";
 		case HARDWARE_PROJECT_K8:  return "odin";
 		case HARDWARE_PROJECT_K2:  return "venus";
+		case HARDWARE_PROJECT_K3S:  return "vili";
 		case HARDWARE_PROJECT_K1A: return "mars";
 		case HARDWARE_PROJECT_K9:  return "renoir";
+		case HARDWARE_PROJECT_K9D:  return "lisa";
 		case HARDWARE_PROJECT_K11:
 			if ( (uint32_t)CountryIndia == get_hw_country_version())
 				return "haydnin";
