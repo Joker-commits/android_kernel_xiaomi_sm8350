4d38578f55975ba49dd4548e9d72383d45b2e3d8
Giovanni Ricca <giovanniricca@proton.me>
2023-02-14 17:35:17 +0100

    input: touchscreen: gt9916: Don't probe if gpio_96 returns 0
    Change-Id: I920c80b3c5b9b78f0d17ab94f918d0194489acd8

--- drivers/input/touchscreen/gt9916/goodix_ts_core.c
+++ drivers/input/touchscreen/gt9916/goodix_ts_core.c
@@ -21,6 +21,7 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/uaccess.h>
+#include <linux/hwid.h>
 
 #include <drm/mi_disp_notifier.h>
 #include <linux/backlight.h>
@@ -3527,7 +3528,17 @@ static int __init goodix_ts_core_init(void)
 	gpio_68 = gpio_get_value(TS_ID_DET2);
 	pr_info("gpio_96 = %d\n",gpio_96);
 	pr_info("gpio_68 = %d\n",gpio_68);
-	pr_info("TP is goodix");
+
+	if (get_hw_version_platform() == HARDWARE_PROJECT_L9) {
+		if (gpio_96) {
+			pr_info("TP is goodix");
+		} else {
+			pr_info("TP is focaltech");
+			return 0;
+		}
+	} else {
+		pr_info("TP is goodix");
+	}
 
 	pr_info("Core layer init:%s", GOODIX_DRIVER_VERSION);
 #ifdef CONFIG_TOUCHSCREEN_GOODIX_BRL_SPI
